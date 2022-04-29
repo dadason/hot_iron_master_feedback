75b6dc9198c81fdb357c69c2b6c064f971ccf948
mamamochka <guidingproject@gmail.com>
2022-04-29 15:01:28 +0300

    29.04 commit

--- hot_iron-master/forms/goods_form.py
+++ hot_iron-master/forms/goods_form.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired
 class GoodsForm(FlaskForm):
     title = StringField('Название', validators=[DataRequired()])
     content = TextAreaField("Описание")
-    category = SelectField("Категория", choices=[(1, 'Ворота'), (2, 'Двери'), (3, 'Решетки')], coerce=int, default=(2, 'Двери'))
+    category = SelectField("Категория", choices=[(1, "Вензеля"), (2, 'Двери'), (3, 'Решетки')], coerce=int, default=(2, 'Двери'))
     # photo = HiddenField("Фото")
     submit = SubmitField('Применить')
--- hot_iron-master/main.py
+++ hot_iron-master/main.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template, redirect, request, abort, url_for
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+import datetime
 
-from forms.goods_form import GoodsForm
-from forms.user_form import RegisterForm, LoginForm
+from flask import Flask, render_template, redirect, request, abort
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_mail import Mail
+
+from data import db_session
 from data.goods import Goods
 from data.users import User
-from data import db_session
-import datetime
-import os
+from forms.goods_form import GoodsForm
+from forms.user_form import RegisterForm, LoginForm
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
+mail = Mail(app)
+menu = {'name': 'обратная связь', 'url': 'feedback'}
+
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'test@gmail.com'  # введите свой адрес электронной почты здесь
+app.config['MAIL_DEFAULT_SENDER'] = 'test@gmail.com'  # и здесь
+app.config['MAIL_PASSWORD'] = 'password'  # введите пароль
 
 
 @login_manager.user_loader
@@ -50,6 +60,7 @@ def goods_delete(id):
 @login_required
 def edit_goods(id):
     form = GoodsForm()
+    photo = ""
     if request.method == "GET":
         db_sess = db_session.create_session()
         goods = db_sess.query(Goods).filter(Goods.id == id, Goods.user == current_user).first()
@@ -60,48 +71,113 @@ def edit_goods(id):
             form.content.data = goods.content
             photo = goods.photo
         else:
-            form.title.data = ""
-            form.content.data = ""
-            photo = None
+            redirect('/add_goods')
+        # form.title.data = ""
+        # form.content.data = ""
+        # photo = None
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        if id:
+        if id > 0:
+            print("id>0")
             goods = db_sess.query(Goods).filter(Goods.id == id).first()
             if goods:
                 goods.title = form.title.data
                 goods.content = form.content.data
-                db_sess.commit()
                 uploaded_file = request.files['file']
                 if uploaded_file.filename != '':
                     uploaded_file.save(f'./static/img/{form.category.data}/{goods.photo}')
-                return redirect(f'/goods/')
+                    goods.photo = uploaded_file.filename
+                    db_sess.commit()
+                return redirect('/')
             else:
                 abort(404)
         else:
-            goods = Goods()
-            goods.title = form.title.data
-            goods.content = form.content.data
-            goods.category_id = form.category.data
-            goods.created_date = datetime.date.today()
-            goods.photo = ""
-            uploaded_file = request.files['file']
-            if uploaded_file.filename != '':
-                goods.photo = uploaded_file.filename
-                uploaded_file.save(f'./static/img/{form.category.data}/{uploaded_file.filename}')
-            current_user.goods.append(goods)
-            db_sess.merge(current_user)
-            db_sess.commit()
-            goods = db_sess.query(Goods.id).order_by(Goods.id.desc()).first()
-            return redirect(f'/goods/{goods.id}')
+            print("id==0")
+            # goods = Goods()
+            # goods.title = form.title.data
+            # goods.content = form.content.data
+            # goods.category_id = form.category.data
+            # goods.created_date = datetime.date.today()
+            # goods.photo = ""
+            # uploaded_file = request.files['file']
+            # if uploaded_file.filename != '':
+            #     goods.photo = uploaded_file.filename
+            #     uploaded_file.save(f'./static/img/{form.category.data}/{uploaded_file.filename}')
+            # current_user.goods.append(goods)
+            # print("goods", goods.id, goods.category_id, goods.photo)
+            # db_sess.merge(current_user)
+            # db_sess.commit()
+            # goods = db_sess.query(Goods.id).order_by(Goods.id.desc()).first()
+            return redirect('/add_goods')
+    return render_template('goods.html', title='Редактировать', form=form, photo=photo)
+
+
+@app.route("/add_goods", methods=['GET', 'POST'])
+def add_goods():
+    form = GoodsForm()
+    if request.method == "GET":
+        # db_sess = db_session.create_session()
+        # goods = db_sess.query(Goods).filter(Goods.id == id, Goods.user == current_user).first()
+        # if goods:
+        #     form.category.default = goods.category_id
+        #     form.process()
+        #     form.title.data = goods.title
+        #     form.content.data = goods.content
+        #     photo = goods.photo
+        # else:
+        form.title.data = ""
+        form.content.data = ""
+        photo = None
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        # получаем номер последнего эелемента таблицы goods
+        goods = db_sess.query(Goods).order_by(Goods.id.desc()).first()
+        id = goods.id + 1
+        # if id > 0:
+        #     print("id>0")
+        #     goods = db_sess.query(Goods).filter(Goods.id == id).first()
+        #     if goods:
+        #         goods.title = form.title.data
+        #         goods.content = form.content.data
+        #         db_sess.commit()
+        #         uploaded_file = request.files['file']
+        #         if uploaded_file.filename != '':
+        #             uploaded_file.save(f'./static/img/{form.category.data}/{goods.photo}')
+        #         return redirect('/goods/')
+        #     else:
+        #         abort(404)
+        # else:
+        print("id=", goods.id)
+        goods = Goods()
+        goods.id = id
+        goods.title = form.title.data
+        goods.content = form.content.data
+        goods.category_id = form.category.data
+        goods.created_date = datetime.date.today()
+        goods.photo = ""
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            goods.photo = uploaded_file.filename
+            uploaded_file.save(f'./static/img/{form.category.data}/{uploaded_file.filename}')
+        current_user.goods.append(goods)
+        print("goods", goods.id, goods.category_id, goods.photo)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        goods = db_sess.query(Goods.id).order_by(Goods.id.desc()).first()
+        return redirect('/')
     return render_template('goods.html', title='Редактировать', form=form, photo=photo)
 
 
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
-    gates = db_sess.query(Goods).filter(Goods.category_id == 1)
+    vensels = db_sess.query(Goods).filter(Goods.category_id == 1)
+    num_vensels = db_sess.query(Goods).filter(Goods.category_id == 1).count()
+    print(num_vensels)
+    num_gratings = db_sess.query(Goods).filter(Goods.category_id == 3).count()
     gratings = db_sess.query(Goods).filter(Goods.category_id == 3)
-    return render_template("navs.html", gates=gates, gratings=gratings)
+    print(num_gratings)
+    return render_template("navs.html", vensels=vensels, gratings=gratings)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -138,6 +214,32 @@ def login():
             return redirect("/")
         return render_template('login.html', message="Неправильный логин или пароль", form=form)
     return render_template('login.html', title='Авторизация', form=form)
+
+
+# @app.route('/contact', methods=['GET', 'POST'])
+# def contact():
+#     #form = ContactForm()
+#     form1 = ContactForm(MultiDict([('name', 'jerry'), ('email', 'jerry@mail.com')]))
+#
+#     # if form.validate_on_submit():
+# 	# name = form.name.data
+# 	# email = form.email.data
+# 	# message = form.message.data
+# 	# print(name)
+# 	# print(email)
+# 	# print(message)
+# 	# # здесь логика базы данных
+# 	# print("\nData received. Now redirecting ...")
+# 	# return redirect(url_for('contact'))
+#
+#     return render_template('contact.html', form=form1)
+#
+#
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+    if request.method == 'POST':
+        print(request.form)
+    return render_template('contact.html', title='Обратная связь', menu=menu)
 
 
 if __name__ == '__main__':
